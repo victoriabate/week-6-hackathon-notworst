a5c3381392a7f690b31a7596e78f673d5d2a8081
maddocash <danksashleigh@gmail.com>
2017-10-13 13:51:45 +0100

    closes #2, closes #3

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,11 +1,19 @@
-// const Post = require(`../models/posts.js`); // uncomment for mongoose
-const express = require(`express`);
-const router = express.Router();
-const path = require('path');
+const Team = require(`../models/teams.js`), // uncomment for mongoose
+      express = require(`express`),
+      router = express.Router(),
+      path = require('path');
 
 /* GET users listing. */
 router.get(`/`, (req, res, next) => {
   res.sendFile(path.join(__dirname,'../views/index.html'));
 });
+
+// router.get('/', function(req, res) {
+//
+//   Team.find({},function(err, docs) {
+//     // res.render(`index`, {posts: docs});
+//     res.send(docs)
+//   });
+// });
 
 module.exports = router;
